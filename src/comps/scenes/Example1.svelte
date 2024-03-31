8c1ad67b39e83430c4585f50167badb14f64dcc8
Albarelli <felipesalbarelli@gmail.com>
2024-03-31 14:47:34 -0300

    add player

--- src/comps/scenes/Example1.svelte
+++ src/comps/scenes/Example1.svelte
@@ -1,10 +1,31 @@
 <script lang="ts">
-  import { T, useTask } from '@threlte/core'
+  import { T, useTask, useThrelte } from '@threlte/core'
   import { interactivity } from '@threlte/extras'
   import { spring } from 'svelte/motion'
   import Ground from '../common/Ground.svelte';
   import { commandStore } from '../ui-dashboard/commandStore';
   import type { HslColor } from '../models';
+  import { AutoColliders, CollisionGroups } from '@threlte/rapier';
+  import Player from '../common/Player.svelte';
+  import { BoxGeometry, Mesh, MeshStandardMaterial, Vector3 } from 'three';
+
+  let playerMesh: Mesh
+  let positionHasBeenSet = false
+  const smoothPlayerPosX = spring(0)
+  const smoothPlayerPosZ = spring(0)
+  const t3 = new Vector3()
+  useTask(() => {
+    if (!playerMesh) return
+    playerMesh.getWorldPosition(t3)
+    smoothPlayerPosX.set(t3.x, {
+      hard: !positionHasBeenSet
+    })
+    smoothPlayerPosZ.set(t3.z, {
+      hard: !positionHasBeenSet
+    })
+    if (!positionHasBeenSet) positionHasBeenSet = true
+  })
+  const { size } = useThrelte()
 
   interactivity()
   const scale = spring(1)
@@ -45,9 +66,46 @@
   <T.MeshStandardMaterial color="hotpink" />
 </T.Mesh>
 
-<Ground 
-  hslColor={ groundColor }
-/>
+<CollisionGroups groups={[0,15]} >
+  <Ground 
+    hslColor={ groundColor }
+  />
+</CollisionGroups>
+
+<CollisionGroups groups={[0]}>
+  <Player
+    bind:playerMesh
+    collGround={[15]}
+    position={[0, 2, -3]}
+  />
+  <!-- WALLS -->
+  <AutoColliders shape={'cuboid'}>
+    <T.Mesh
+      receiveShadow
+      castShadow
+      position.x={30 + 0.7 + 0.15}
+      position.y={1.275}
+      geometry={new BoxGeometry(60, 2.55, 0.15)}
+      material={new MeshStandardMaterial({
+        transparent: true,
+        opacity: 0.5,
+        color: 0x333333
+      })}
+    />
+    <T.Mesh
+      receiveShadow
+      castShadow
+      position.x={-30 - 0.7 - 0.15}
+      position.y={1.275}
+      geometry={new BoxGeometry(60, 2.55, 0.15)}
+      material={new MeshStandardMaterial({
+        transparent: true,
+        opacity: 0.5,
+        color: 0x333333
+      })}
+    />
+  </AutoColliders>
+</CollisionGroups>
 
 <!-- <T.Mesh
   rotation.x={-Math.PI / 2}
